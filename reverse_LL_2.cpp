141ffcdfae75133c0cc295d5d888211320280620
Gagandeep <gagandeepkaur1523@gmail.com>
2023-08-01 21:57:25 +0530

    Modified reverse linked list

--- reverse_LL_2.cpp
+++ reverse_LL_2.cpp
@@ -82,4 +82,48 @@ int main() {
 }
 
 
+/*
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(head==NULL || head->next==NULL || left==right){
+            return head;
+        }
+
+        ListNode* temp=new ListNode();
+        temp->next=head;
+        ListNode* prev=temp;
+
+        for(int i=0; i<left-1; i++){
+            prev=prev->next;
+        }
+
+        ListNode* curr=prev->next;
+        ListNode* next_node=curr->next;
+
+        for(int i=0; i<right-left;i++){
+            curr->next=next_node->next;
+            next_node->next=prev->next;
+            prev->next=next_node;
+            next_node=curr->next;
+        }
+
+        head=temp->next;
+        delete temp;
+
+        return head;
+    }
+};
+*/
+
+
 
